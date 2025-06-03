2bd161e612653dbe709ca0aee170926826473fff
Jpns2002 <joaopnascsales@gmail.com>
2025-06-03 11:56:41 -0300

    adicao_das_funcoes_para_coletar_dados

--- configs/xpath_options.py
+++ configs/xpath_options.py
@@ -1,5 +1,16 @@
 xpath_options_dict = {
     'game_time': list(([
-    '/html/body/div/main/div[2]/div/div[3]/div/div/div/div[1]/div/div/div[1]/div[2]/h1',
+    '/html/body/div/main/div[2]/div/div[3]/div/div/div/div[1]/div/div/div[1]/div[2]/h1'
+])),
+    'game_date': list(([
+        '/html/body/div/main/div[2]/div/div[1]/div[2]'
 
-]))}
+])),
+
+    'team_gold_blue_side': list(([
+        '/html/body/div/main/div[2]/div/div[3]/div/div/div/div[1]/div/div/div[2]/div[1]/div[2]/div[5]/span/'
+    ])),
+    'team_gold_red_side': list(([
+        '/html/body/div/main/div[2]/div/div[3]/div/div/div/div[1]/div/div/div[2]/div[1]/div[2]/div[5]/span/'
+    ]))
+}
--- src/main.py
+++ src/main.py
@@ -8,10 +8,18 @@ from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 # from urllib.parse import urlparse
 import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import pandas as pd
 from configs.xpath_options import xpath_options_dict
 import logging
 
+#Função para iniciar o webdriver
+# inicializa o driver como None para evitar erros de escopo
+
+
+
+# Configuração do logging
 logging.basicConfig(
     level=logging.INFO,
     format=r"%(asctime)s [%(levelname)s] %(message)s",
@@ -36,7 +44,6 @@ os.makedirs(LOG_DIR, exist_ok=True)
 
 # Caminho completo para o arquivo de log
 LOG_PATH = os.path.join(LOG_DIR, "scraper.log")
-
 
 # Inicializa o navegador com o driver atualizado automaticamente
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
@@ -65,9 +72,9 @@ game.click()
 def get_game_time ():
     """Função para obter o tempo do jogo."""
 
-    xpath_options = xpath_options_dict.get("game_time", [])
+    xpath_options = xpath_options_dict.get("game_time", []) #xpath_options_dict é um dicionário que contém as opções de XPath para localizar o tempo do jogo
 
-    for xpath in xpath_options:
+    for xpath in xpath_options: # percorre as opções de XPath
         try:
             element = driver.find_element(By.XPATH, xpath)
             game_time = element.text.strip()
@@ -81,21 +88,72 @@ def get_game_time ():
             raise
 
 
+#função para coletar a data do jogo
+def get_game_date():
+    """Função para obter a data do jogo"""
+
+    xpath_opitons = xpath_options_dict.get("game_date", []) # obtém as opções de XPath para a data do jogo
+    for xpath in xpath_opitons: # percorre as opções de XPath
+        try:
+            element = driver.find_element(By.XPATH, xpath) # tenta encontrar o elemento pelo XPath
+            game_date = element.text.strip() # obtém o texto do elemento e remove espaços em branco
+            if game_date: # se o texto não estiver vazio
+                game_date = game_date.split(' ', 1)
+                game_week = game_date[1]
+                game_week = game_week.strip('()')
+                game_date = game_date[0]
+
+                return game_date, game_week # retorna a data do jogo
+        except NoSuchElementException:
+            logging.error(f"[game_date] XPath não encontrado: {xpath}")
+            raise
+        except Exception as e:
+            logging.error(f"[game_date] Erro ao buscar a data do jogo: {e}")
+            raise
+# Obtém a data do jogo
+
+def get_gold_red_side():
+    """Função para obter o ouro do lado vermelho."""
+
+    xpath_options= xpath_options_dict.get("team_gold_red_side", [])
+    for xpath in xpath_options:
+        try:
+            wait = WebDriverWait(driver, 5)
+            element = wait.until(EC.presence_of_element_located((By.XPATH, xpath))) # espera até que o elemento esteja presente na página
+            gold_red_side = element.text.strip()
+
+            if gold_red_side: # se o texto não estiver vazio
+                gold_red_side = gold_red_side.replace(',', '')
+                print(f"[DEBUG - GOLD RED SIDE] {gold_red_side}")
+                return gold_red_side
+        except NoSuchElementException:
+            logging.error(f"[gold_red_side] XPath não encontrado: {xpath}")
+            raise
+        except Exception as e:
+            logging.error(f"[gold_red_side] Erro ao buscar o ouro do lado vermelho: {e}")
+            raise
+
 def scrape_game_data():
     """Função principal para coletar os dados do jogo."""
 
     game_time = get_game_time()
+    game_date, game_week = get_game_date()
+    gold_red_side = get_gold_red_side()
 
-    data = pd.DataFrame(columns=['game_uid', 'game_time'],
-                        data=[[game_uid, game_time]])
+    # TODO: refatorar para adicionar os headers caso não existam
+    data = pd.DataFrame(columns=['game_uid', 'game_time','game_date','game_week'], # cria um DataFrame com as colunas especificadas
+                        data=[[game_uid, game_time, game_date, game_week]]) # adiciona os dados coletados ao DataFrame
 
-    data.to_csv(CSV_PATH, mode='a', header=not os.path.exists(CSV_PATH), index=False, encoding='utf-8')
+    data.to_csv(CSV_PATH, mode='a', header=not os.path.exists(CSV_PATH), index=False, encoding='utf-8') # escreve os dados no CSV
 
-    with open(CSV_PATH, 'r', encoding='utf-8') as file:
+    with open(CSV_PATH, 'r', encoding='utf-8') as file: # lê o arquivo CSV
         last_line = file.readlines()[-1].strip()
 
     logging.info(f"[DEBUG - ÚLTIMA LINHA CSV] {last_line}")
 
 
+
 scrape_game_data()
 # time.sleep(5)
+
+
